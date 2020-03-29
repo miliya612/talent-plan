1d8449683d8f03e9c7b8b6bac50a34fb70769336
Tomoki <t.miliya612@gmail.com>
2020-03-29 15:18:54 +0900

    read environment variable

--- rust/works/building-blocks/bb-01/meow/src/main.rs
+++ rust/works/building-blocks/bb-01/meow/src/main.rs
@@ -4,9 +4,16 @@ extern crate clap;
 extern crate dotenv_codegen;
 
 use clap::App;
+use std::env;
 
 fn main() {
     println!("{}", dotenv!("PORT"));
+
+    let key = "HOME";
+    match env::var_os(key) {
+        Some(val) => println!("{}: {:?}", key, val),
+        None => println!("{} is not defined in the environment.", key),
+    }
 
     let yml = load_yaml!("cli.yml");
     let _ = App::from_yaml(yml).get_matches();
