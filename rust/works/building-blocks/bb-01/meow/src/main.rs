e1c0a9b650b2a8700c8c3dcaaa00884b3c6ca039
Tomoki <t.miliya612@gmail.com>
2020-03-29 15:17:57 +0900

    introduce dotenv

--- rust/works/building-blocks/bb-01/meow/src/main.rs
+++ rust/works/building-blocks/bb-01/meow/src/main.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate dotenv_codegen;
 
 use clap::App;
 
 fn main() {
+    println!("{}", dotenv!("PORT"));
+
     let yml = load_yaml!("cli.yml");
     let _ = App::from_yaml(yml).get_matches();
 }
