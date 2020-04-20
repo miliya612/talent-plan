2d118a333274b10d934a46ba1c6c476b07d7f6de
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-04-21 01:57:10 +0900

    ex: serde_bson

--- rust/works/building-blocks/bb-02/serde_bson/src/main.rs
+++ rust/works/building-blocks/bb-02/serde_bson/src/main.rs
@@ -1,7 +1,7 @@
 use bson;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io::BufReader;
+use std::io::prelude::*;
 use std::io::BufWriter;
 use std::io::Cursor;
 use std::str;
@@ -12,13 +12,38 @@ struct Move {
 }
 
 fn main() {
-    let b = bson::to_bson(&Move { squares: 1 }).unwrap();
-    let b = b.as_document().unwrap();
-    println!("bson: {:?}", b);
-    let mut frw = File::create("bson").unwrap();
-    bson::encode_document(&mut frw, b).unwrap();
-    let fro = File::open("bson").unwrap();
-    let mut r = BufReader::new(fro);
-    let d = bson::decode_document(&mut r).unwrap();
-    println!("decoded: {:?}", d);
+    let f = File::create("bson").unwrap();
+
+    let b: Vec<u8> = Vec::new();
+    let mut c = Cursor::new(b);
+
+    for i in 0..100 {
+        let bs = bson::to_bson(&Move { squares: i }).unwrap();
+        let bs = bs.as_document().unwrap();
+        bson::encode_document(&mut c, bs).unwrap();
+    }
+
+    let mut writer = BufWriter::new(f);
+    writer.write(c.into_inner().as_slice()).unwrap();
+    match writer.flush() {
+        Ok(()) => (),
+        Err(e) => panic!(e),
+    }
+
+    let mut f = File::open("bson").unwrap();
+    let mut b: Vec<u8> = Vec::new();
+    f.read_to_end(&mut b).unwrap();
+    let mut c = Cursor::new(b);
+    let mut buf = vec![0; 18];
+
+    loop {
+        match c.read(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => {
+                let d = bson::decode_document(&mut buf.as_slice()).unwrap();
+                println!("decoded: {:?}", d);
+            }
+            Err(e) => panic!(e),
+        }
+    }
 }
