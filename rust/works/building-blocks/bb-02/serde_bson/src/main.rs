4756f5b771a0749d232ff0308ab7512b98fe8f5c
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-04-21 21:40:05 +0900

    separate method for file and vec8 impl

--- rust/works/building-blocks/bb-02/serde_bson/src/main.rs
+++ rust/works/building-blocks/bb-02/serde_bson/src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::io::Cursor;
+use std::io::SeekFrom;
 use std::str;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -12,8 +13,10 @@ struct Move {
 }
 
 fn main() {
-    let f = File::create("bson").unwrap();
+    main_with_vec8()
+}
 
+fn main_with_vec8() {
     let b: Vec<u8> = Vec::new();
     let mut c = Cursor::new(b);
 
@@ -23,19 +26,10 @@ fn main() {
         bson::encode_document(&mut c, bs).unwrap();
     }
 
-    let mut writer = BufWriter::new(f);
-    writer.write(c.into_inner().as_slice()).unwrap();
-    match writer.flush() {
-        Ok(()) => (),
-        Err(e) => panic!(e),
-    }
-
-    let mut f = File::open("bson").unwrap();
-    let mut b: Vec<u8> = Vec::new();
-    f.read_to_end(&mut b).unwrap();
-    let mut c = Cursor::new(b);
+    // TODO: handle the size with better way
     let mut buf = vec![0; 18];
 
+    c.seek(SeekFrom::Start(0)).unwrap();
     loop {
         match c.read(&mut buf) {
             Ok(0) => break,
@@ -47,3 +41,35 @@ fn main() {
         }
     }
 }
+
+fn main_with_file() {
+    let f = File::create("bson").unwrap();
+    let mut writer = BufWriter::new(f);
+
+    for i in 0..100 {
+        let bs = bson::to_bson(&Move { squares: i }).unwrap();
+        let bs = bs.as_document().unwrap();
+        bson::encode_document(&mut writer, bs).unwrap();
+    }
+
+    match writer.flush() {
+        Ok(()) => (),
+        Err(e) => panic!(e),
+    }
+
+    let mut f = File::open("bson").unwrap();
+    // TODO: handle the size with better way
+    let mut buf = vec![0; 18];
+
+    loop {
+        match f.read(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => {
+                let d = bson::decode_document(&mut buf.as_slice()).unwrap();
+                println!("decoded: {:?}", d);
+            }
+            Err(e) => panic!(e),
+        }
+    }
+}
+
